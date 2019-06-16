b85f48db4a5f84c2f17ccebf846bdc06e1c82f08
Emmanuel <emmopk11@outlook.com>
2019-06-16 14:05:58 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+require('dotenv').config();
+
+module.exports = {
+  context: path.join(__dirname, 'src'),
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: './bundle.js',
+    publicPath: '/',
+  },
+  devServer: {
+    historyApiFallback: true,
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(jpg|png|jpeg|gif)$/,
+        loader: 'file-loader',
+      },
+      {
+        test: /\.(css|scss)$/,
+        loader: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: 'body',
+      template: './index.html',
+    }),
+  ],
+};
