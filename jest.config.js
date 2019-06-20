86cf852db30abd8e8a15661c484c46e6d1cf5ef1
Emmanuel <emmopk11@outlook.com>
2019-06-20 23:46:41 +0200

    Add configuration for enzyme

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,5 @@ module.exports = {
     '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
     '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js',
   },
+  setupFiles: ['<rootDir>/src/__tests___/index.js'],
 };
--- src/__tests__/MovieDetailsViewSpec.js
+++ src/__tests__/MovieDetailsViewSpec.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-import { BrowserRouter } from 'react-router-dom';
+import { shallow } from 'enzyme';
+import { Route, BrowserRouter } from 'react-router-dom';
 import { MovieDetailsView } from '../components/movie-details-view/MovieDetailsView';
 
 
 describe('<MovieDetailsView />', () => {
-  it('Should not Break When no Movie is passed', () => {
-    const component = renderer.create(<BrowserRouter><MovieDetailsView /></BrowserRouter>);
+  it('Should not Break When Movie id is passed', () => {
+    const component = shallow(
+      <BrowserRouter>
+        <Route path="movie/:5332-testmovie" component={MovieDetailsView} />
+      </BrowserRouter>,
+    );
     const tree = component.toJSON();
 
     expect(tree).toMatchSnapshot();
-  });
-
-  it('Should Correctly Render When Movie is passed', () => {
-
   });
 });
